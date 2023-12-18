766b113e3616fca9288b979dd5bf8a5c6c9d8fc4
MMUWEI <1821370389@qq.com>
2023-12-18 11:52:55 +0800

    [修改] 新增了动态数组的API接口的形参

--- dynamicArray/dynamicArray.h
+++ dynamicArray/dynamicArray.h
@@ -12,30 +12,33 @@ typedef struct dynamicArray
 /* API ：application pragram interface */
 
 /* 动态数组初始化 */
-int dynamicArrayInit(dynamicArray *dynamicArray, int capacity);
+int dynamicArrayInit(dynamicArray *pArray);
 
 /* 动态数组插入数据(默认插入到数组的末尾) */
-int dynamicArrayInsertData(dynamicArray *dynamicArray, ElemType value);
+int dynamicArrayInsertData(dynamicArray *pArray, ElemType value);
 
 /* 动态数组插入数据(在指定位置) */
-int dynamicArrayAppointPosInsertData(dynamicArray *dynamicArray, ElemType value, int index);
+int dynamicArrayAppointPosInsertData(dynamicArray *pArray, ElemType value, int pos);
 
 /* 动态数组修改指定位置的数据 */
-int dynamicArrayModifyAppointPosData(dynamicArray *dynamicArray, ElemType value, int index);
+int dynamicArrayModifyAppointPosData(dynamicArray *pArray, ElemType value, int pos);
 
 /* 动态数组删除数据(默认删除末尾的数据) */
-int dynamicArrayDeleteData(dynamicArray *dynamicArray);
+int dynamicArrayDeleteData(dynamicArray *pArray);
 
 /* 动态数组删除数据(指定位置的数据) */
-int dynamicArrayDeleteAppointPosData(dynamicArray *dynamicArray, int index);
+int dynamicArrayDeleteAppointPosData(dynamicArray *pArray, int pos);
+
+/* 动态数组删除数据(指定数据)*/
+int dynamicArrayDeleteAppointData(dynamicArray *pArray, ElemType value);
 
 /* 动态数组的销毁 */
-int dynamicArrayDestroy(dynamicArray *dynamicArray);
+int dynamicArrayDestroy(dynamicArray *pArray);
 
 /* 获取动态数组的大小 */
-int dynamicArrayGetSize(dynamicArray *dynamicArray);
+int dynamicArrayGetSize(dynamicArray *pArray, int *pSize);
 
 /* 获取动态数组的容量 */
-int dynamicArrayGetCapacity(dynamicArray *dynamicArray);
+int dynamicArrayGetCapacity(dynamicArray *pArray, int *pCapacity);
 
 #endif  //__DYNAMIC_ARRAY_H__
