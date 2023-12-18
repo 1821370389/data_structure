4a118c83508515479482992f99efc1c1394ecc8d
MMUWEI <1821370389@qq.com>
2023-12-18 11:19:22 +0800

    [修改] 新增动态数组的结点

--- dynamicArray/dynamicArray.c
+++ dynamicArray/dynamicArray.c
@@ -1,7 +1,3 @@
 #include <stdio.h>
 #include "dynamicArray.h"
 
-int main() 
-{
-    
-}
--- dynamicArray/dynamicArray.h
+++ dynamicArray/dynamicArray.h
@@ -1,6 +1,13 @@
 #ifndef __DYNAMIC_ARRAY_H__
 #define __DYNAMIC_ARRAY_H__
 /* 避免头文件重命名*/
+typedef int ElemType;
+typedef struct dynamicArray
+{
+    ElemType *data; /* 数组的空间*/
+    int size;       /* 数组的大小 */
+    int capacity;   /* 数组的容量 */
+}dynamicArray;
 
 
 #endif  //__DYNAMIC_ARRAY_H__
