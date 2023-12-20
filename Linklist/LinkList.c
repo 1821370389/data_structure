152bf37ee3f3d0f9958413d463099d5c1bb22a66
MMUWEI <1821370389@qq.com>
2023-12-20 10:13:58 +0800

    [修改] 在.c中添加声明

---
+++ Linklist/LinkList.c
@@ -0,0 +1,67 @@
+#include "LinkList.h"
+
+/* 链表的初始化*/
+int LinkListInit(LinkList **pList)
+{
+
+}
+
+/* 链表头插 */
+int LinkListHeadInsert(LinkList *pList, ELEMENTTYPE data)
+{
+    
+}
+
+/* 链表尾插 */
+int LinkListTailInsert(LinkList *pList, ELEMENTTYPE data)
+{
+    
+}
+
+/* 链表指定位置插入 */
+int LinkListInsert(LinkList *pList, int pos, ELEMENTTYPE data)
+{
+    
+}
+
+/* 链表头删 */
+int LinkListHeadDelete(LinkList *pList)
+{
+    
+}
+
+/* 链表尾删 */
+int LinkListTailDelete(LinkList *pList)
+{
+    
+}
+
+/* 链表指定位置删除 */
+int LinkListDelete(LinkList *pList, int pos)
+{
+    
+}
+
+/* 链表指定值删除 */
+int LinkListDeleteByValue(LinkList *pList, ELEMENTTYPE data)
+{
+    
+}
+
+/* 获取链表长度 */
+int LinkListLen(LinkList *pList, int *pLen)
+{
+    
+}
+
+/* 获取链表指定位置的值 */
+int LinkListGet(LinkList *pList, int pos, ELEMENTTYPE *data)
+{
+    
+}
+
+/* 链表销毁 */
+int LinkListDestroy(LinkList *pList)
+{
+    
+}
--- Linklist/LinkList.h
+++ Linklist/LinkList.h
@@ -28,37 +28,37 @@ typedef struct LinkList
 } LinkList;
 
 /* 链表的初始化*/
-int LinkListInit(LinkList *list);
+int LinkListInit(LinkList **pList);
 
 /* 链表头插 */
-int LinkListHeadInsert(LinkList *list, ELEMENTTYPE data);
+int LinkListHeadInsert(LinkList *pList, ELEMENTTYPE data);
 
 /* 链表尾插 */
-int LinkListTailInsert(LinkList *list, ELEMENTTYPE data);
+int LinkListTailInsert(LinkList *pList, ELEMENTTYPE data);
 
 /* 链表指定位置插入 */
-int LinkListInsert(LinkList *list, ELEMENTTYPE data, int pos);
+int LinkListInsert(LinkList *pList, int pos, ELEMENTTYPE data);
 
 /* 链表头删 */
-int LinkListHeadDelete(LinkList *list);
+int LinkListHeadDelete(LinkList *pList);
 
 /* 链表尾删 */
-int LinkListTailDelete(LinkList *list);
+int LinkListTailDelete(LinkList *pList);
 
 /* 链表指定位置删除 */
-int LinkListDelete(LinkList *list, int pos);
+int LinkListDelete(LinkList *pList, int pos);
 
 /* 链表指定值删除 */
-int LinkListDeleteByValue(LinkList *list, ELEMENTTYPE data);
+int LinkListDeleteByValue(LinkList *pList, ELEMENTTYPE data);
 
 /* 获取链表长度 */
-int LinkListLen(LinkList *list);
+int LinkListLen(LinkList *pList, int *pLen);
 
 /* 获取链表指定位置的值 */
-int LinkListGet(LinkList *list, int pos, ELEMENTTYPE *data);
+int LinkListGet(LinkList *pList, int pos, ELEMENTTYPE *data);
 
 /* 链表销毁 */
-int LinkListDestroy(LinkList *list);
+int LinkListDestroy(LinkList *pList);
 
 
 #endif
