27ff988f93d16d28a2a71151596e289392d6ac3e
MMUWEI <1821370389@qq.com>
2023-12-25 09:33:54 +0800

    [修改] 修改栈函数的参数

---
+++ dynamicArrayStack/dynamicArrayStack.c
@@ -0,0 +1,38 @@
+#include "dynamicArrayStack.h"
+
+/* 栈的初始化 */
+int dynamicArrayStackInit(dynamicArrayStack * pStack)
+{
+    
+}
+
+/* 压栈 */
+int dynamicArrayStackPush(dynamicArrayStack * pStack,ElemType value)
+{
+    
+}
+/* 查看栈顶元素 */
+int dynamicArrayStackTop(dynamicArrayStack * pStack, ElemType * pValue)
+{
+    
+}
+/* 出栈 */
+int dynamicArrayStackPop(dynamicArrayStack * pStack)
+{
+    
+}
+/* 获得栈的大小 */
+int dynamicArrayStackSize(dynamicArrayStack * pStack, ElemType * pSize)
+{
+    
+}
+/* 判断栈是否为空 */
+int dynamicArrayStackEmpty(dynamicArrayStack * pStack)
+{
+    
+}
+/* 栈的销毁 */
+int dynamicArrayStackDestroy(dynamicArrayStack * pStack)
+{
+
+}
--- dynamicArrayStack/dynamicArrayStack.h
+++ dynamicArrayStack/dynamicArrayStack.h
@@ -2,25 +2,28 @@
 #define __DYNAMIC_ARRAY_STACK_H__
 /* 避免头文件重命名*/
 
+#include "dynamicArray.h"
+typedef dynamicArray dynamicArrayStack;
+
 /* 栈的初始化 */
-int dynamicArrayStackInit(int capacity);
+int dynamicArrayStackInit(dynamicArrayStack * pStack);
 
 /* 压栈 */
-int dynamicArrayStackPush(int value);
+int dynamicArrayStackPush(dynamicArrayStack * pStack,ElemType value);
 
 /* 查看栈顶元素 */
-int dynamicArrayStackTop();
+int dynamicArrayStackTop(dynamicArrayStack * pStack,ElemType* pValue);
 
 /* 出栈 */
-int dynamicArrayStackPop();
+int dynamicArrayStackPop(dynamicArrayStack * pStack);
 
 /* 获得栈的大小 */
-int dynamicArrayStackSize();
+int dynamicArrayStackSize(dynamicArrayStack * pStack);
 
 /* 判断栈是否为空 */
-int dynamicArrayStackEmpty();
+int dynamicArrayStackEmpty(dynamicArrayStack * pStack, int* pSize);
 
 /* 栈的销毁 */
-int dynamicArrayStackDestroy();
+int dynamicArrayStackDestroy(dynamicArrayStack * pStack);
 
 #endif  //__DYNAMIC_ARRAY_H__
